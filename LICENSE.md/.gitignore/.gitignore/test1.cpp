1a1ec41ec3b78723fdb35235df4f57eaf8466271
sbfhy <381921675@qq.com>
2018-03-15 12:04:03 +0800

    Update and rename .gitignore to test1.cpp

---
+++ LICENSE.md/.gitignore/.gitignore/test1.cpp
@@ -0,0 +1,21 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+#pragma comment(linker, "/STACK:102400000,102400000")
+#define rep(i,a,b) for (int i=a; i<=b; ++i)
+#define per(i,b,a) for (int i=b; i>=a; --i)
+#define mes(a,b)  memset(a,b,sizeof(a))
+#define INF 0x3f3f3f3f
+#define MP make_pair
+#define PB push_back
+#define fi  first
+#define se  second
+typedef long long ll;
+const int N = 200005;
+
+int main()
+{
+
+
+    return 0;
+}
